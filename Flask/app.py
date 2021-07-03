a8ce65f6d42e56f06a11391852d65820a869e04c
owenmckenney <owen.mckenney1@gmail.com>
2021-07-03 17:24:52 +0100

    updated app and index

--- Flask/app.py
+++ Flask/app.py
@@ -5,5 +5,19 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route('/xposypos/<string:pos>', methods=['POST'])
+def processPos(pos):
+    position = json.loads(pos)
+    print()
+    print("x position: " + str(position['x']))
+    print("y position: " + str(position['y']))
+    print()
+
+    with open('pos.txt', 'a') as f:
+        f.write("position: " + str(position['x']) + ", " + str(position['y']))
+        f.close()
+
+    return "done"
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=80, debug=True)
